e84c71f8622ec1dee0a5905ee865787818fcc120
Saqib Shaikh <Me@SaqibShaikh.com>
2011-12-04 15:43:05 +0000

    Adding package

---
+++ BlackBall/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
@@ -0,0 +1,15 @@
+using System.Data.Entity;
+using BlackBall.Models;
+using Devtalk.EF.CodeFirst;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(BlackBall.App_Start.DontDropDbJustCreateTablesIfModelChangedStart), "Start")]
+
+namespace BlackBall.App_Start {
+    public static class DontDropDbJustCreateTablesIfModelChangedStart {
+        public static void Start() {
+            // Uncomment this line and replace CONTEXT_NAME with the name of your DbContext if you are 
+            // using your DbContext to create and manage your database
+            Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<BlackBallDatabase>());
+        }
+    }
+}
